7d0a932c9af9e016bf22bfbbb0fa89ef45834d4f
MichalArmon <armonmic22003@gmail.com>
2026-02-13 12:53:55 +0200

    c

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Container, Typography, Paper, TextField, Button } from "@mui/material";
+
+import "./App.css";
+
+function App() {
+  return <Button>fgssfsff</Button>;
+}
+
+export default App;
